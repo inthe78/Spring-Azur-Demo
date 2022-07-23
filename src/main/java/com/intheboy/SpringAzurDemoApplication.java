122dc6cb801ad8e98605af60ea72939d1b9fc69c
inthe78 <isayas077@gmail.com>
2022-07-23 15:30:41 -0600

    first commit

---
+++ src/main/java/com/intheboy/SpringAzurDemoApplication.java
@@ -0,0 +1,13 @@
+package com.intheboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzurDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzurDemoApplication.class, args);
+	}
+
+}
